4180419c1f86ef9703eb5ffb15a8f6f17006f9dd
kfarooq1981 <148969272+kfarooq1981@users.noreply.github.com>
2026-03-12 17:15:47 +0500

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['typeorm', 'better-sqlite3', 'reflect-metadata'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
